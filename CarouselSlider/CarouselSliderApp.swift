5777fb682d9c8954bfcf7d74152186f3f08334b1
Max <maxim.macari.dam@gmail.com>
2021-01-18 16:42:21 +0100

    Initial Commit

---
+++ CarouselSlider/CarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSliderApp.swift
+//  CarouselSlider
+//
+//  Created by Maxim Macari on 18/1/21.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
